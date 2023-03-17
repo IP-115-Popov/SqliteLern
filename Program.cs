c0ca48edca7ac5cf4915d9d287c7cb9c84ad70ce
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-03-17 19:11:45 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace SqliteLeaen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection("Data Source=bdLern2.db"))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = "CREATE TABLE Users(_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, Name TEXT NOT NULL, Age INTEGER NOT NULL)";
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("Таблица Users создана");
+            }
+            Console.Read();
+        }
+    }
+}
